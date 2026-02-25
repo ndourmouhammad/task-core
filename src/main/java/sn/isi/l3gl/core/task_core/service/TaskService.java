5aa02dc4bcfd13755b071f64017a7fea22460094
ndourmouhammad <ndourmouhammad15@gmail.com>
2026-02-25 07:30:18 +0000

    feat: implement updateStatus functionality (v0.2.0-SNAPSHOT)

--- src/main/java/sn/isi/l3gl/core/task_core/service/TaskService.java
+++ src/main/java/sn/isi/l3gl/core/task_core/service/TaskService.java
@@ -2,6 +2,7 @@ package sn.isi.l3gl.core.task_core.service;
 
 import org.springframework.stereotype.Service;
 import sn.isi.l3gl.core.task_core.entite.Task;
+import sn.isi.l3gl.core.task_core.entite.TaskStatus;
 import sn.isi.l3gl.core.task_core.repository.TaskRepository;
 
 import java.util.List;
@@ -23,5 +24,12 @@ public class TaskService {
     // Ajout dans la Version 0.1.0-SNAPSHOT
     public List<Task> listTasks() {
         return taskRepository.findAll();
+    }
+    // Ajout dans la Version 0.2.0-SNAPSHOT
+    public Task updateStatus(Long id, TaskStatus newStatus) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tâche introuvable avec l'ID : " + id));
+        task.setStatus(newStatus);
+        return taskRepository.save(task);
     }
 }
